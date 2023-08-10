42e89d4bf316e086d9811fc6c21ac76db0e6c1ba
Pongking <429750130@qq.com>
2023-08-10 10:25:09 +0800

    fix the unittest to mydict

--- unitest_exp/unittest_mydict.py
+++ unitest_exp/unittest_mydict.py
@@ -3,16 +3,16 @@ from my_dict import MyDict
 
 class TestDict(unittest.TestCase):
     def setUp(self):
-        self.keys={'a','b','c','d'}
-        self.values={'1','2','3','4'}
+        self.keys=['a','b','c','d']
+        self.values=['1','2','3','4']
         self.dict=MyDict()
     
     def testDict(self):
-        for i in range(0,len(self.keys)):
-            self.dict.put(self.keys.pop(),self.values.pop())
-        for i in range(0,len(self.keys)):
-            self.assertEqual(self.dict.get(self.keys[i]),self.values[i])
-
+        for k,v in zip(self.keys,self.values):
+            self.dict.put(k,v)
+        for key,value in zip(self.keys,self.values):
+            self.assertEqual(self.dict.get(key),value)
+        
 
 if __name__ =='__main__':
     unittest.main()
