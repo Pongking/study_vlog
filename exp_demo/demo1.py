7c545f352ad0d4147ad02b96dc9ed9e09377e914
Pongking <429750130@qq.com>
2023-09-04 19:47:37 +0800

    update exp_demo

---
+++ exp_demo/demo1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+my_list=[1,2,3,4,5]
+
+my_array=np.array(my_list)
+
+nested_list=[[1,2,3],[4,5,6],[7,8,9]]
+
+my_2d_array=np.array(nested_list)
+#print(my_2d_array)
+zeros_array=np.zeros(5)
+ones_array=np.ones((3,3))
+#print(zeros_array)
+#print("---------")
+#print(ones_array)
+my_range=np.arange(10)
+
+print(ones_array[0])
