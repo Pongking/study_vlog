137b261260178009039cbea6ac2f1bd9d83f234b
Pongking <429750130@qq.com>
2023-10-15 22:21:38 +0800

    add feedforward and backpropagation

--- shell_ts/day1.sh
+++ shell_ts/day1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 #use ` to assign the result of the formula to variable 
+#like to define a function
+
 suffix=`date +%Y%m%d` 
 
-for f in `find ./data/ -type f -name "*.txt_"`
+for f in `find ./data/ -type f -name "*.txt"`
 do
+    echo "backup the file ${f}"
     echo "backup the file $f"
     cp ${f} ${f}_${suffix}
 done
