911651f903b6165b4269a2ee0040e80b08149c26
Pongking <429750130@qq.com>
2023-11-07 21:12:39 +0800

    upload

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,9 @@
-description=$1
+name=$1
+description=$2
+
+cp ~/.ssh/id_rsa_{$1} ~/.ssh/id_rsa 
+cp ~/.ssh/id_rsa_{$1}.pub ~/.ssh/id_rsa.pub 
 
 git add .
-git commit -m "$1"
+git commit -m "$2"
 git push -u origin main
