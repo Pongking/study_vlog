19c566c134de7900f8395be121621fa8b2f0af23
Pongking <429750130@qq.com>
2023-11-20 19:55:11 +0800

    upload the code style

---
+++ deeplearning/fun_code_style.py
@@ -0,0 +1,18 @@
+
+
+def load_data():
+    # 加载数据的代码
+    return 0
+
+def clean_data(data):
+    # 清理数据的代码
+    return 0
+def analyze_data(data):
+    # 分析数据的代码
+    return 0
+def visualize_data(data):
+    # 可视化数据的代码
+    return 0
+
+
+
